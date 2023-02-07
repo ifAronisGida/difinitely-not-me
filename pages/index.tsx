827714d2ea1834df6aab0a43f8180d29d4572338
a.gida <a.gida@mortoff.hu>
2023-02-07 19:46:51 +0100

    added font awesome to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home({
   allPostsData
@@ -21,6 +23,7 @@ export default function Home({
       </Head>
       <section className="text-xl">
         <p className="text-small m-5 text-center">Automating my front to self!</p>
+        <div><FontAwesomeIcon icon={faEnvelope} size="sm"></FontAwesomeIcon></div>
       </section>
       <section>
         <h2>Blog</h2>
