2019375637d4ff01d2f222fc075703980fda43bd
a.gida <a.gida@mortoff.hu>
2023-02-13 21:30:32 +0100

    added tsparticles for bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
-import '@fortawesome/fontawesome-svg-core/styles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
