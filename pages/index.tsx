cd649aaf4eba4b1d67faa6fb8ba785cec64e7895
a.gida <a.gida@mortoff.hu>
2023-02-14 19:18:11 +0100

    mobile style fixing

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ export default function Home() {
         <AnimatedBackground />
       </div>
 
-      <section id="home" className="sm:text-xl content-center">
+      <section id="home" className="text-sm sm:text-xl content-center">
 
         <div className="place-items-center min-h-screen grid grid-cols-1">
           <div className="text-center min-w-max">
-            <div className="max-w-md">
+            <div className="max-w-xs sm:max-w-md">
               <h1 className="text-xs sm:text-xl font-thin">Hello there!</h1>
               <h1 className="text-sm sm:text-5xl font-bold">My name is Áron</h1>
               <p className="py-6">I am motivated by creating useful and nice things. I automate processes as a profession and build software in my free time with passion.</p>
@@ -31,10 +31,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="experience" className="sm:text-xl">
-        <div className="hero min-h-screen">
+      <section id="experience" className="text-sm sm:text-xl content-center">
+        <div className="min-h-screen place-items-center grid grid-cols-1">
           <div className="text-center">
-            <div className="max-w-xl xl:max-w-6xl">
+            <div className="max-w-xs sm:max-w-6xl">
               <h1 className="mb-5 text-5xl font-bold">Experiences:</h1>
               <div className="grid gap-4 grid-cols-1 xl:grid-cols-3">
                 <Experiences />
@@ -43,10 +43,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="projects" className="sm:text-xl">
-        <div className="hero min-h-screen">
+      <section id="projects" className="text-sm sm:text-xl content-center">
+        <div className="min-h-screen place-items-center grid grid-cols-1">
           <div className="min-w-max text-center">
-            <div className="max-w-xl">
+            <div className="max-w-xs sm:max-w-xl">
               <h1 className="text-5xl font-bold mb-5">Projects:</h1>
               <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <Projects />
